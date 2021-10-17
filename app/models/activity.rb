c3168ee861787460337f814147c9979afd74a838
João Batista Silva <joao@linux.com>
2021-10-16 23:45:08 -0400

    Validações em atividades

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,27 @@
 class Activity < ApplicationRecord
   belongs_to :project
+
+  validates_presence_of :name, message: "Nome da atividade não pode estar vazio"
+  validates_presence_of :start_date, message: "Data de início precisa ser informada"
+  validates_presence_of :end_date, message: "Não foi informada data de término"
+  validate :end_date_after_start_date  
+  
+  private
+      
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?  
+    if end_date < start_date
+      errors.add(:end_date, "Data de término não pode ser menor que data de início")
+    end
+
+    return if project.blank?
+
+    if start_date < project.start_date
+      errors.add(:start_date, "Data de início da atividade não pode ser menor que data de início do projeto")
+    end
+
+    if end_date < project.start_date
+      errors.add(:end_date, "Data de término da atividade não pode ser menor que data de início do projeto")
+    end    
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,28 +1,31 @@
 class Project < ApplicationRecord
-    validates_presence_of :name, message: "Nome do projeto não pode estar vazio"
-    validates_presence_of :start_date, message: "Data inicial do projeto precisa ser informada"
-    validates_presence_of :end_date, message: "Não foi informada data de término do projeto"
-    validate :end_date_after_start_date
+  has_many :activities
 
-    def delayed
-        false
-    end
+  validates_presence_of :name, message: "Nome do projeto não pode estar vazio"
+  validates_presence_of :start_date, message: "Data de início precisa ser informada"
+  validates_presence_of :end_date, message: "Não foi informada data de término"
+  validate :end_date_after_start_date
+  
 
-    def finished
-        false
-    end
+  def delayed
+    false
+  end
 
-    # sobrescrevo o as_json inserindo os dois novos 'campos' no resultado
-    def as_json(options={})
-        super(methods: [:delayed, :finished])
-    end
+  def finished
+    false
+  end
 
-    private
+  # sobrescrevo o as_json inserindo os dois novos 'campos' no resultado
+  def as_json(options={})
+      super(methods: [:delayed, :finished])
+  end
+
+  private
       
-    def end_date_after_start_date
-        return if end_date.blank? || start_date.blank?  
-        if end_date < start_date
-            errors.add(:end_date, "Data de término não pode ser menor que data de início")
-        end
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?  
+    if end_date < start_date
+      errors.add(:end_date, "Data de término não pode ser menor que data de início")
     end
+  end
 end
